35af17dbd4208177db0bb70211db71d31eee5e8c
Dhxbxb <oneno9674@gmail.com>
2025-10-20 12:01:21 +0700

    A skeleton on which to build a microservice architecture

---
+++ hello-service/src/main/java/com/pet/hello_service/HelloController.java
@@ -0,0 +1,14 @@
+package com.pet.hello_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+    @GetMapping
+    public String sayHello(){
+        return "Hello from hello-service! (via Gateway & Consul)";
+    }
+}
---
+++ hello-service/src/main/java/com/pet/hello_service/HelloServiceApplication.java
@@ -0,0 +1,15 @@
+package com.pet.hello_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class HelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceApplication.class, args);
+	}
+
+}
